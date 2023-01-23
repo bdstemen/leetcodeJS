254093d2c464108293e3c6afcdb819f4ae2da7ee
Ben Stemen <ben.d.stemen@gmail.com>
2023-01-23 10:59:09 -0500

    3sum

---
+++ 15-3sum.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+ var threeSum = function(nums) {
+  let result = [];
+  let sum, l, r;
+  nums.sort((a, b) => a - b);
+  for (let i = 0; i < nums.length - 2; i++) {
+      if (nums[i] === nums[i - 1]) continue;
+
+      l = i + 1;
+      r = nums.length - 1;
+
+      while (l < r) {
+          sum = nums[l] + nums[r] + nums[i];
+
+          if (sum === 0) {
+              result.push([nums[l], nums[r], nums[i]]);
+              while (nums[l] + nums[r] + nums[i] === sum) {
+                  l++;
+              }
+          } else if (sum > 0) {
+              while (nums[l] + nums[r] + nums[i] === sum) {
+                  r--;
+              }
+          } else {
+              while (nums[l] + nums[r] + nums[i] === sum) {
+                  l++;
+              }
+          }
+      }
+  }
+  return result;
+};
