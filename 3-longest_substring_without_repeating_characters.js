15fc53a748a2db3062efff620e2724963f8c87bf
Ben Stemen <ben.d.stemen@gmail.com>
2023-01-28 17:29:06 -0500

    longest substring without repeating characters using set

--- 3-longest_substring_without_repeating_characters.js
+++ 3-longest_substring_without_repeating_characters.js
@@ -1,19 +1,38 @@
+// /**
+//  * @param {string} s
+//  * @return {number}
+//  */
+//  var lengthOfLongestSubstring = function(s) {
+//   let char, index;
+//   let sub = '';
+//   let max = 0;
+//   for (let i = 0; i < s.length; i++) {
+//       char = s[i];
+//       sub += char;
+//       index = sub.indexOf(char);
+//       if (index !== sub.length - 1) {
+//           sub = sub.slice(index + 1);
+//       }
+//       max = Math.max(max, sub.length);
+//   }
+//   return max;
+// };
+
 /**
  * @param {string} s
  * @return {number}
  */
- var lengthOfLongestSubstring = function(s) {
-  let char, index;
-  let sub = '';
-  let max = 0;
-  for (let i = 0; i < s.length; i++) {
-      char = s[i];
-      sub += char;
-      index = sub.indexOf(char);
-      if (index !== sub.length - 1) {
-          sub = sub.slice(index + 1);
-      }
-      max = Math.max(max, sub.length);
-  }
-  return max;
+var lengthOfLongestSubstring = function(s) {
+    let l = 0;
+    let max = 0;
+    let sub = new Set();
+    for (let r = 0; r < s.length; r++) {
+        while (sub.has(s[r])) {
+            sub.delete(s[l]);
+            l++;
+        }
+        sub.add(s[r]);
+        max = Math.max(max, sub.size);
+    }
+    return max;
 };
