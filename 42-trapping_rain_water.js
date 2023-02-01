b7d323ad76c074a541dc04c29785ea8a2d34d1d7
Ben Stemen <ben.d.stemen@gmail.com>
2023-02-01 18:38:29 -0500

    optimize trap rain water solution

--- 42-trapping_rain_water.js
+++ 42-trapping_rain_water.js
@@ -2,6 +2,11 @@
  * @param {number[]} height
  * @return {number}
  */
+
+// BASIC SOLN
+// O(n) time
+// O(n) space
+
 var trap = function(height) {
   let maxL = Array(height.length);
   let maxR = Array(height.length);
@@ -26,4 +31,29 @@ var trap = function(height) {
   }
 
   return totalWater;
+};
+
+
+// OPTIMIZED SOLN (two pointers)
+// O(n) time
+// O(1) space
+
+var trap = function(height) {
+    let water = 0;
+    let maxL = 0;
+    let maxR = 0;
+    let l = 0;
+    let r = height.length - 1;
+    while (l < r) {
+        if (height[l] <= height[r]) {
+            water += Math.max((maxL - height[l]), 0);
+            maxL = Math.max(maxL, height[l]);
+            l++;
+        } else {
+            water += Math.max((maxR - height[r]), 0);
+            maxR = Math.max(maxR, height[r]);
+            r--;
+        }
+    }
+    return water;
 };
