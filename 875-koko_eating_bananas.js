8ba6d1a750de68e7ee688069fe2c302ba7ae0f8d
Ben Stemen <ben.d.stemen@gmail.com>
2023-02-07 16:45:22 -0500

    koko eats bananas optimized

--- 875-koko_eating_bananas.js
+++ 875-koko_eating_bananas.js
@@ -13,3 +13,28 @@ var minEatingSpeed = function(piles, h) {
       if (hours <= h) return k;
   }
 };
+
+
+// OPTIMIZED SOLN
+var minEatingSpeed = function(piles, h) {
+  let r = Math.max(...piles);
+  let l = 1;
+
+  while (l < r) {
+      let m = Math.floor((l + r) / 2);
+      if (eatBananas(piles, m) <= h) {
+          r = m;
+      } else {
+          l = m + 1;
+      }
+  }
+  return l;
+};
+
+var eatBananas = function(piles, k) {
+  let hours = 0;
+  for (let i = 0; i < piles.length; i++) {
+      hours += Math.ceil(piles[i] / k);
+  }
+  return hours;
+};
