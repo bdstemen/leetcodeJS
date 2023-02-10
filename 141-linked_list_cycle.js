6edc7ae5fda6fc8dac59ecbb6fd7e2a8e542349b
Ben Stemen <ben.d.stemen@gmail.com>
2023-02-10 10:41:06 -0500

    linked list cycle nieve approace

---
+++ 141-linked_list_cycle.js
@@ -0,0 +1,12 @@
+// Initial approach
+// O(n) time & space
+var hasCycle = function(head) {
+  let nodes = new Set();
+  while (head) {
+      if (nodes.has(head)) return true;
+      nodes.add(head);
+      head = head.next;
+  }
+  return false;
+};
+
