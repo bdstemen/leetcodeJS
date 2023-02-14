eed853f85538c0e49716edf3bceb3d904a1da031
Ben Stemen <ben.d.stemen@gmail.com>
2023-02-14 17:25:02 -0500

    pure recursive same tree

--- 100-same_tree.js
+++ 100-same_tree.js
@@ -29,3 +29,28 @@ var isSameTree = function(p, q) {
   dfs(p, q);
   return result;
 };
+
+
+// Pure Recursive DFS
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+  if (!p && !q) {
+      return true;
+  } else if (!p || !q || p.val !== q.val) {
+      return false;
+  }
+
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
