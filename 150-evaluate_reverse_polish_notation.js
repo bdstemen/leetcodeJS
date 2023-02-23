2283bf4bd17d7cace678665eae5fd34c92daee24
Ben Stemen <ben.d.stemen@gmail.com>
2023-02-23 13:20:18 -0500

    optimized evaluate reverse polish notation

--- 150-evaluate_reverse_polish_notation.js
+++ 150-evaluate_reverse_polish_notation.js
@@ -24,3 +24,26 @@ var evalRPN = function(tokens) {
 
     return vals[0]
 };
+
+// optimized
+var evalRPN = function(tokens) {
+    let stack = [];
+
+    for (let i = 0; i < tokens.length; i++) {
+        let curr = tokens[i];
+
+        if (!isNaN(parseInt(curr))) {
+            stack.push(parseInt(curr));
+        } else {
+            let val1 = stack.pop();
+            let val2 = stack.pop();
+
+            if (curr === '+') stack.push(val2 + val1);
+            else if (curr === '-') stack.push(val2 - val1);
+            else if (curr === '*') stack.push(val2 * val1);
+            else stack.push(Math.trunc(val2 / val1));
+        }
+    }
+
+    return stack[0]
+};
