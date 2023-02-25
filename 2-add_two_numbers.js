ed9a28d45ad0c9fc9cad4b8fb269735638fb0b55
Ben Stemen <ben.d.stemen@gmail.com>
2023-02-25 11:47:01 -0500

    add two numbers

---
+++ 2-add_two_numbers.js
@@ -0,0 +1,39 @@
+var addTwoNumbers = function(l1, l2) {
+    let sum = 0;
+    let val1, val2;
+    let result = new ListNode();
+    let end = result;
+
+    while (l1 || l2) {
+        if (l1) {
+            val1 = l1.val;
+            l1 = l1.next;
+        } else {
+            val1 = 0;
+        }
+
+        if (l2) {
+            val2 = l2.val;
+            l2 = l2.next;
+        } else {
+            val2 = 0;
+        }
+
+        sum = (val1 + val2);
+
+        if (end.val > 9) {
+            sum += Math.trunc(end.val / 10);
+            end.val = end.val % 10;
+        }
+
+        end.next = new ListNode(sum, null);
+        end = end.next;
+    }
+
+    if (end.val > 9) {
+        end.next = new ListNode(Math.trunc(end.val / 10));
+        end.val = end.val % 10;
+    }
+
+    return result.next;
+};
