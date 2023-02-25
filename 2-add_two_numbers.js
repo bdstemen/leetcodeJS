b449172ec9339de6961cc8cc079c51bfb8690a36
Ben Stemen <ben.d.stemen@gmail.com>
2023-02-25 12:04:39 -0500

    clean add two numbers solution

--- 2-add_two_numbers.js
+++ 2-add_two_numbers.js
@@ -37,3 +37,29 @@ var addTwoNumbers = function(l1, l2) {
 
     return result.next;
 };
+
+// cleaner solution
+var addTwoNumbers = function(l1, l2) {
+    let sum = 0;
+    let carry = 0;
+    let val1, val2;
+    let result = new ListNode();
+    let end = result;
+
+    while (l1 || l2 || carry) {
+        val1 = l1 ? l1.val : 0;
+        val2 = l2 ? l2.val : 0;
+
+        sum = (val1 + val2 + carry);
+        carry = Math.trunc(sum / 10);
+        sum = sum % 10;
+
+        end.next = new ListNode(sum, null);
+
+        end = end.next;
+        l1 = l1 ? l1.next : null;
+        l2 = l2 ? l2.next : null;
+    }
+
+    return result.next;
+};
