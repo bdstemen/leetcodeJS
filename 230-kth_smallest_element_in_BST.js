7eade93bab4e1499b1f4cafcc1b9cf5dea44a5ca
Ben Stemen <ben.d.stemen@gmail.com>
2023-02-27 12:57:48 -0500

    kth smallest element of BST recursive solution

--- 230-kth_smallest_element_in_BST.js
+++ 230-kth_smallest_element_in_BST.js
@@ -1,3 +1,4 @@
+// iterative solution using stack
 var kthSmallest = function(root, k) {
     let n = 0;
     let stack = [];
@@ -14,4 +15,21 @@ var kthSmallest = function(root, k) {
         if (n === k) return curr.val;
         curr = curr.right;
     }
+};
+
+// Recursive solution
+var kthSmallest = function(root, k) {
+    let count = 1;
+    let result;
+
+    let search = function(curr) {
+        if (!curr) return;
+        search(curr.left);
+        if (count === k) result = curr.val;
+        count++;
+        search(curr.right);
+    }
+
+    search(root);
+    return result;
 };
