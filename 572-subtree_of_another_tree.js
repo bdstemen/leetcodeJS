b82d9a8ada802db460c1dc4781edae38e4d0b07c
Ben Stemen <ben.d.stemen@gmail.com>
2023-03-07 09:50:35 -0500

    optimize subtree of another tree

--- 572-subtree_of_another_tree.js
+++ 572-subtree_of_another_tree.js
@@ -2,7 +2,7 @@ var isSubtree = function(root, subRoot) {
     if (!subRoot) return true;
     if (!root) return false;
 
-    if (isSameTree(root, subRoot)) return true;
+    if (root.val === subRoot.val && isSameTree(root, subRoot)) return true;
 
     return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
 };
