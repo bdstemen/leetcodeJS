306cc92ed00fd2601fc7df4c79ecfa94ead3d194
Ben Stemen <ben.d.stemen@gmail.com>
2023-03-09 10:27:09 -0500

    minimum add to make parentheses valid cleaner solution

--- 921-minimum_add_to_make_parenthesis_valid.js
+++ 921-minimum_add_to_make_parenthesis_valid.js
@@ -19,3 +19,20 @@ var minAddToMakeValid = function(s) {
 
     return inserts + stack.length;
 };
+
+// cleaner solution
+var minAddToMakeValid = function(s) {
+  let stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+      let curr = s[i];
+
+      if (curr === ')' && stack[stack.length - 1] === '(') {
+          stack.pop();
+      } else {
+          stack.push(curr);
+      }
+  }
+
+  return stack.length;
+};
