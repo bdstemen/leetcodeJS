447c049919bc4319038d7568b6cf9c54b50d6692
Ben Stemen <ben.d.stemen@gmail.com>
2023-03-09 09:44:08 -0500

    update binary tree right side view solution

--- 199-binary_tree_right_side_view.js
+++ 199-binary_tree_right_side_view.js
@@ -6,14 +6,13 @@ var rightSideView = function(root) {
 
     while (queue.length) {
         let len = queue.length;
-        result.push(queue[len - 1].val);
-
-        for (let i = len - 1; i > -1; i--) {
+        result.push(queue[len - 1].val)
+        for (let i = 0; i < len; i++) {
             let curr = queue.shift();
             if (curr.left) queue.push(curr.left);
-            if (curr.right) queue. push(curr.right);
+            if (curr.right) queue.push(curr.right);
         }
     }
 
-    return result;
+    return result
 };
