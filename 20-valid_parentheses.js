9fe6c79df934f8294982095580274d686750aa72
Ben Stemen <ben.d.stemen@gmail.com>
2023-03-09 10:01:02 -0500

    valid parentheses

--- 20-valid_parentheses.js
+++ 20-valid_parentheses.js
@@ -1,7 +1,3 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
 var isValid = function(s) {
   let stack = [];
   for (let i = 0; i < s.length; i++) {
@@ -12,5 +8,32 @@ var isValid = function(s) {
       else if (paren === '}' && stack[stack.length - 1] === '{') stack.pop();
       else return false;
   }
+  return stack.length === 0;
+};
+
+
+var isValid = function(s) {
+  let parens = {
+      '(': ')',
+      '[': ']',
+      '{': '}'
+  };
+
+  let stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+      let curr = s[i];
+
+      if (parens.hasOwnProperty(curr)) {
+          stack.push(curr);
+          continue;
+      }
+
+      let top = stack.pop();
+      if (parens[top] !== curr) {
+          return false;
+      }
+  }
+
   return stack.length === 0;
 };
