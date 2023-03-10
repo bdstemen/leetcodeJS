9694c9c9cc6f47b4a3a698fe90d025ca400d7c4e
Ben Stemen <ben.d.stemen@gmail.com>
2023-03-10 18:37:45 -0500

    update binary search solution

--- 704-binary_search.js
+++ 704-binary_search.js
@@ -1,20 +1,14 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number}
- */
-
 // RECURSIVE APPROACH
 
-// var search = function(nums, target, l = 0, r = nums.length - 1) {
-//     if (l > r) return -1;
+var search = function(nums, target, l = 0, r = nums.length - 1) {
+    if (l > r) return -1;
 
-//     let m = Math.floor((l + r) / 2);
+    let m = Math.floor((l + r) / 2);
 
-//     if (nums[m] === target) return m;
-//     else if (nums[m] > target) return search(nums, target, l, m - 1);
-//     else return search(nums, target, m + 1, r);
-// };
+    if (nums[m] === target) return m;
+    else if (nums[m] > target) return search(nums, target, l, m - 1);
+    else return search(nums, target, m + 1, r);
+};
 
 // ITERATIVE APPROACH
 
