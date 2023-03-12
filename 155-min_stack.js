c67563aaab9d38700e423c1e59fdd2ee1d94cc41
Ben Stemen <ben.d.stemen@gmail.com>
2023-03-12 16:48:19 -0400

    clean up min stack solution"

--- 155-min_stack.js
+++ 155-min_stack.js
@@ -1,64 +1,42 @@
-// var MinStack = function() {
-//   this.stack = [];
-//   this.minIndex = [];
-// };
+// initial solution
+var MinStack = function() {
+  this.stack = [];
+  this.minIndex = [];
+};
 
-// /**
-// * @param {number} val
-// * @return {void}
-// */
-// MinStack.prototype.push = function(val) {
-//   if (this.minIndex.length && val < this.stack[this.minIndex[this.minIndex.length - 1]]) {
-//       this.minIndex.push(this.stack.length);
-//   } else if (this.minIndex.length === 0) {
-//       this.minIndex.push(0)
-//   }
+MinStack.prototype.push = function(val) {
+  if (this.minIndex.length && val < this.stack[this.minIndex[this.minIndex.length - 1]]) {
+      this.minIndex.push(this.stack.length);
+  } else if (this.minIndex.length === 0) {
+      this.minIndex.push(0)
+  }
 
-//   this.stack.push(val);
-// };
+  this.stack.push(val);
+};
 
-// /**
-// * @return {void}
-// */
-// MinStack.prototype.pop = function() {
-//   if (this.minIndex[this.minIndex.length - 1] === this.stack.length - 1) {
-//       this.minIndex.pop();
-//   }
-//   return this.stack.pop()
-// };
+MinStack.prototype.pop = function() {
+  if (this.minIndex[this.minIndex.length - 1] === this.stack.length - 1) {
+      this.minIndex.pop();
+  }
+  return this.stack.pop()
+};
 
-// /**
-// * @return {number}
-// */
-// MinStack.prototype.top = function() {
-//   return this.stack[this.stack.length - 1];
-// };
+MinStack.prototype.top = function() {
+  return this.stack[this.stack.length - 1];
+};
 
-// /**
-// * @return {number}
-// */
-// MinStack.prototype.getMin = function() {
-//   return this.stack[this.minIndex[this.minIndex.length - 1]];
-// };
+MinStack.prototype.getMin = function() {
+  return this.stack[this.minIndex[this.minIndex.length - 1]];
+};
 
-// /**
-// * Your MinStack object will be instantiated and called as such:
-// * var obj = new MinStack()
-// * obj.push(val)
-// * obj.pop()
-// * var param_3 = obj.top()
-// * var param_4 = obj.getMin()
-// */
 
+
+// cleaner solution
 var MinStack = function() {
   this.stack = [];
   this.minStack = [];
 };
 
-/**
-* @param {number} val
-* @return {void}
-*/
 MinStack.prototype.push = function(val) {
   let minStackEmpty = !this.minStack.length;
   let hasNewMin = val <= this.top(this.minStack)
@@ -68,9 +46,6 @@ MinStack.prototype.push = function(val) {
   this.stack.push(val);
 };
 
-/**
-* @return {void}
-*/
 MinStack.prototype.pop = function(stack = this.stack, minStack = this.minStack ) {
   let pop = stack.pop();
   if (this.getMin() === pop) {
@@ -78,25 +53,10 @@ MinStack.prototype.pop = function(stack = this.stack, minStack = this.minStack )
   }
 };
 
-/**
-* @return {number}
-*/
 MinStack.prototype.top = function(stack = this.stack) {
   return stack.length ? stack[stack.length - 1] : null;
 };
 
-/**
-* @return {number}
-*/
 MinStack.prototype.getMin = function(minStack = this.minStack) {
   return this.top(minStack);
 };
-
-/**
-* Your MinStack object will be instantiated and called as such:
-* var obj = new MinStack()
-* obj.push(val)
-* obj.pop()
-* var param_3 = obj.top()
-* var param_4 = obj.getMin()
-*/
