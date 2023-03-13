0e2e70400eb0e56dcbd1d49c59de9c47b16bdcf9
Ben Stemen <ben.d.stemen@gmail.com>
2023-03-13 11:17:33 -0400

    clean up coinchange solution

--- 322-coin_change.js
+++ 322-coin_change.js
@@ -1,15 +1,14 @@
 var coinChange = function(coins, amount) {
-    let dp = Array(amount + 1).fill(amount + 1);
-    dp[0] = 0;
+  let dp = Array(amount + 1).fill(amount + 1);
+  dp[0] = 0;
 
-    for (let a = 1; a < dp.length; a++) {
-        for (let i = 0; i < coins.length; i++) {
-            let c = coins[i];
-            if (a - c >= 0) {
-                dp[a] = Math.min(dp[a], 1 + dp[a - c]);
-            }
-        }
-    }
+  for (let a = 1; a < dp.length; a++) {
+      coins.forEach(c => {
+          if (a - c >= 0) {
+              dp[a] = Math.min(dp[a], 1 + dp[a - c]);
+          }
+      })
+  }
 
-    return (dp[amount] !== amount + 1) ? dp[amount] : -1;
+  return (dp[amount] !== amount + 1) ? dp[amount] : -1;
 };
