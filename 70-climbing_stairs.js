9f2fadf5bc62bbd65b2f682b5337577ad59e8699
Ben Stemen <ben.d.stemen@gmail.com>
2023-03-13 10:02:13 -0400

    climbing stairs DP

--- 70-climbing_stairs.js
+++ 70-climbing_stairs.js
@@ -13,3 +13,18 @@ var climbStairs = function(n, curr = 0) {
 
   return ways;
 };
+
+// Dynamic Programming method
+var climbStairs = function(n) {
+    let temp;
+    let one = 1;
+    let two = 1;
+
+    for (let i = 0; i < n - 1; i++) {
+        temp = one;
+        one = one + two;
+        two = temp;
+    }
+
+    return one;
+};
