c64161a6c42207c3401c25df79a301616ccc9981
Ben Stemen <ben.d.stemen@gmail.com>
2023-03-14 18:09:27 -0400

    optimized rotated sorted array solution:

--- 33-search_in_rotated_sorted_array.js
+++ 33-search_in_rotated_sorted_array.js
@@ -68,3 +68,25 @@ const findTarget = (array, left, right, target) => {
     else if (target < array[middle]) return findTarget(array, left, middle - 1, target);
     else if (target > array[middle]) return findTarget(array, middle + 1, right, target);
 };
+
+// optimized iterative solution
+var search = function(nums, target) {
+    let l = 0;
+    let r = nums.length - 1;
+
+    while (l <= r) {
+        let m = Math.floor((l + r) / 2);
+
+        if (nums[m] === target) {
+            return m
+        } else if (nums[l] <= nums[m]) {
+            if (nums[l] <= target && nums[m] > target) r = m - 1;
+            else l = m + 1;
+        } else {
+            if (nums[m] < target && nums[r] >= target) l = m + 1;
+            else r = m - 1;
+        }
+    }
+
+    return -1;
+};
