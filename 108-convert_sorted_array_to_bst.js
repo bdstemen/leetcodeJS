45d31d49979e44517661410783705f77bd96eedd
Ben Stemen <ben.d.stemen@gmail.com>
2023-03-23 09:54:46 -0400

    convert sorted array to BST pure recursive soln

--- 108-convert_sorted_array_to_bst.js
+++ 108-convert_sorted_array_to_bst.js
@@ -11,3 +11,13 @@ var sortedArrayToBST = function(nums) {
 
     return assembleTree(0, nums.length - 1);
 };
+
+// pure recursive solution
+var sortedArrayToBST = function(nums, l = 0, r = nums.length - 1) {
+    if (l > r) return null;
+    let mid = Math.floor((l + r) / 2);
+    let node = new ListNode(nums[mid]);
+    node.left = sortedArrayToBST(nums, l, mid - 1);
+    node.right = sortedArrayToBST(nums, mid + 1, r);
+    return node;
+};
