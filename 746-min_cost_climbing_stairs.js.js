0ad68240992d1123dbfea3ce29698c2673ab1d6f
Ben Stemen <ben.d.stemen@gmail.com>
2023-03-26 17:54:21 -0400

    min cost climbing stairs

---
+++ 746-min_cost_climbing_stairs.js.js
@@ -0,0 +1,12 @@
+// DP approach
+var minCostClimbingStairs = function(cost) {
+    let n = cost.length;
+    let totalCost = Array(n);
+    [totalCost[n - 2], totalCost[n - 1]] = [cost[n - 2], cost[n - 1]];
+
+    for (let i = totalCost.length - 3; i >= 0; i--) {
+        totalCost[i] = cost[i] + Math.min(totalCost[i + 1], totalCost[i + 2]);
+    }
+
+    return Math.min(totalCost[0], totalCost[1]);
+};
