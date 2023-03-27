096e943cc956b7d15ba1f2b5992017122a6b60ec
Ben Stemen <ben.d.stemen@gmail.com>
2023-03-27 17:15:51 -0400

    car fleets stack solution

--- 853-car_fleet.js
+++ 853-car_fleet.js
@@ -20,3 +20,22 @@ var carFleet = function(target, position, speed) {
 
     return fleets;
 };
+
+// cleaner solution using stack
+var carFleet = function(target, position, speed) {
+    let cars = Array(position.length);
+    let fleets = [];
+
+    for (let i = 0; i < cars.length; i++) {
+        cars[i] = [position[i], speed[i]];
+    }
+
+    cars.sort((a, b) => (a[0] - b[0]));
+
+    for (let i = cars.length - 1; i >= 0; i--) {
+        let curr = ((target - cars[i][0]) / cars[i][1]);
+        if (fleets.length === 0 || curr > fleets[fleets.length - 1]) fleets.push(curr);
+    }
+
+    return fleets.length;
+};
