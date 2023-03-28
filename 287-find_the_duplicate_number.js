1c89fda55b1a56cf58c80428a5025691b8aabaf5
Ben Stemen <ben.d.stemen@gmail.com>
2023-03-28 11:34:25 -0400

    find the duplicate number brute force soln time complexity

--- 287-find_the_duplicate_number.js
+++ 287-find_the_duplicate_number.js
@@ -1,4 +1,4 @@
-
+// brute force solution O(n^2) time O(1) space
 var findDuplicate = function(nums) {
     for (let i = 0; i < nums.length - 1; i++) {
         for (let j = i + 1; j < nums.length; j++) {
