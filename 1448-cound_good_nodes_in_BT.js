6b2004cf0d6ee6ca677b5f556ae197a51abd22dc
Ben Stemen <ben.d.stemen@gmail.com>
2023-03-29 12:52:53 -0400

    pure recursive soln for count good nodes

--- 1448-cound_good_nodes_in_BT.js
+++ 1448-cound_good_nodes_in_BT.js
@@ -11,3 +11,15 @@ var goodNodes = function(root) {
     traverse(root, root.val);
     return goodNodes;
 };
+
+// pure recursive solution
+var goodNodes = function(root, maxVal = root.val) {
+  let good = 0;
+
+  if (!root) return good;
+
+  else if (root.val >= maxVal) good++;
+  good = good + goodNodes(root.left, Math.max(root.val, maxVal)) + goodNodes(root.right, Math.max(root.val, maxVal));
+
+  return good;
+};
