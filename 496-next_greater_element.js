12360c1e066f7e24d3c64b3c22b585b7fe331394
Ben Stemen <ben.d.stemen@gmail.com>
2023-03-29 11:32:12 -0400

    next greater element solution using stack

--- 496-next_greater_element.js
+++ 496-next_greater_element.js
@@ -13,3 +13,31 @@ var nextGreaterElement = function(nums1, nums2) {
     }
     return result;
 };
+
+
+var nextGreaterElement = function(nums1, nums2) {
+    let result = Array(nums1.length);
+    let hash = {};
+    let stack = [];
+
+    for (let i = 0; i < nums1.length; i++) {
+        hash[nums1[i]] = -1;
+    }
+
+    for (let i = 0; i < nums2.length; i++) {
+        while (stack.length && nums2[i] > stack[stack.length - 1]) {
+            hash[stack[stack.length - 1]] = nums2[i];
+            stack.pop();
+        }
+
+        if (hash.hasOwnProperty(nums2[i])) {
+            stack.push(nums2[i]);
+        }
+    }
+
+    for (let i = 0; i < result.length; i++) {
+        result[i] = hash[nums1[i]];
+    }
+
+    return result;
+};
