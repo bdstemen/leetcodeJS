4aff762a8e28bcf1fbd72e21066828b1f2a2a087
Ben Stemen <ben.d.stemen@gmail.com>
2023-03-30 17:52:49 -0400

    longest substring without repeating characters

--- 3-longest_substring_without_repeating_characters.js
+++ 3-longest_substring_without_repeating_characters.js
@@ -1,27 +1,21 @@
-// /**
-//  * @param {string} s
-//  * @return {number}
-//  */
-//  var lengthOfLongestSubstring = function(s) {
-//   let char, index;
-//   let sub = '';
-//   let max = 0;
-//   for (let i = 0; i < s.length; i++) {
-//       char = s[i];
-//       sub += char;
-//       index = sub.indexOf(char);
-//       if (index !== sub.length - 1) {
-//           sub = sub.slice(index + 1);
-//       }
-//       max = Math.max(max, sub.length);
-//   }
-//   return max;
-// };
 
-/**
- * @param {string} s
- * @return {number}
- */
+ var lengthOfLongestSubstring = function(s) {
+  let char, index;
+  let sub = '';
+  let max = 0;
+  for (let i = 0; i < s.length; i++) {
+      char = s[i];
+      sub += char;
+      index = sub.indexOf(char);
+      if (index !== sub.length - 1) {
+          sub = sub.slice(index + 1);
+      }
+      max = Math.max(max, sub.length);
+  }
+  return max;
+};
+
+//
 var lengthOfLongestSubstring = function(s) {
     let l = 0;
     let max = 0;
