a0e0f875a34dd5daee8c45b85353ade6d1fd8943
Ben Stemen <ben.d.stemen@gmail.com>
2023-04-01 18:42:45 -0400

    subsets II without cache

--- 90-subsets_II.js
+++ 90-subsets_II.js
@@ -18,3 +18,31 @@ var subsetsWithDup = function(nums) {
     backtrack(0, [], {});
     return powerSet;
 };
+
+// approach without using cache
+var subsetsWithDup = function(nums) {
+    nums = nums.sort((a, b) => a - b);
+    let powerSet = [];
+
+    let backtrack = function(i, curr) {
+        if (i === nums.length) {
+            powerSet.push(curr);
+            return;
+        }
+
+        if (nums[i + 1] === nums[i]) {
+            let j = i;
+            while (nums[j] === nums[i]) {
+                j++
+            }
+            backtrack(j, curr);
+        } else {
+            backtrack(i + 1, curr);
+        }
+
+        backtrack(i + 1, curr.concat(nums[i]));
+    }
+
+    backtrack(0, []);
+    return powerSet;
+};
