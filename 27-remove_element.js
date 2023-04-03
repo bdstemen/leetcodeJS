2ec83ea88f14242082ba284967016a71fa6850d9
Ben Stemen <ben.d.stemen@gmail.com>
2023-04-03 10:40:34 -0400

    remove element time complexities

--- 27-remove_element.js
+++ 27-remove_element.js
@@ -1,3 +1,4 @@
+// O(n^2) time - splice is O(n) operation
 var removeElement = function(nums, val) {
     let i = 0;
     while (i < nums.length) {
@@ -10,6 +11,8 @@ var removeElement = function(nums, val) {
     return nums.length;
 };
 
+
+// O(n) time - single pass through array
 var removeElement = function(nums, val) {
   let j = 0;
   for (let i = 0; i < nums.length; i++) {
