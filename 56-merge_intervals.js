36df60f04b5b12a11d1bae706e0c46c5a638c49a
Ben Stemen <ben.d.stemen@gmail.com>
2023-04-08 11:52:29 -0400

    optimized merge intervals solution

--- 56-merge_intervals.js
+++ 56-merge_intervals.js
@@ -1,24 +1,19 @@
+// optimized solution
 var merge = function(intervals) {
-    let result = [];
-    let curr = 0;
+  intervals.sort((a, b) => (a[0] - b[0]));
+  let result = [intervals[0]];
 
-    intervals.sort((a, b) => (a[0] - b[0]));
-    console.log(intervals);
+  for (let i = 1; i < intervals.length; i++) {
+      let prevEnd = result[result.length - 1][1];
+      let currStart = intervals[i][0];
+      let currEnd = intervals[i][1]
 
-    while (curr < intervals.length) {
-        let l = intervals[curr][0];
-        let r = intervals[curr][1];
+      if (prevEnd >= currStart) {
+          result[result.length - 1][1] = Math.max(prevEnd, currEnd);
+      } else {
+          result.push(intervals[i]);
+      }
+  }
 
-        while (curr < intervals.length - 1 && intervals[curr + 1][0] <= r) {
-            curr++;
-            if (intervals[curr][1] > r) {
-                r = intervals[curr][1];
-            }
-        }
-
-        result.push([l, r]);
-        curr++;
-    }
-
-    return result;
+  return result;
 };
