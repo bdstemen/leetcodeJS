becdae2eb37b200277190881fefec385240e8e21
Ben Stemen <ben.d.stemen@gmail.com>
2023-04-08 13:34:36 -0400

    find missing numbers alternate solution

--- 448-find_all_numbers_disappeared_in_an_array.js
+++ 448-find_all_numbers_disappeared_in_an_array.js
@@ -1,3 +1,4 @@
+// cyclic sort solution
 var findDisappearedNumbers = function(nums) {
     let results = [];
 
@@ -11,6 +12,23 @@ var findDisappearedNumbers = function(nums) {
 
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] !== i + 1) results.push(i + 1);
+    }
+
+    return results;
+};
+
+
+// alternate solution
+var findDisappearedNumbers = function(nums) {
+    let results = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        let j = Math.abs(nums[i]) - 1;
+        nums[j] = -1 * Math.abs(nums[j]);
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > 0) results.push(i + 1);
     }
 
     return results;
