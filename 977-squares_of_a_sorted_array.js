add221e457a43f0aae27593e04efd42092879258
Ben Stemen <ben.d.stemen@gmail.com>
2023-04-08 11:03:17 -0400

    square sorted array linear time solution

--- 977-squares_of_a_sorted_array.js
+++ 977-squares_of_a_sorted_array.js
@@ -1,3 +1,23 @@
 var sortedSquares = function(nums) {
     return nums.map(num => num * num).sort((a, b) => (a - b));
 };
+
+
+
+var sortedSquares = function(nums) {
+  let result = Array(nums.length - 1);
+  let l = 0;
+  let r = nums.length - 1;
+
+  for (let i = nums.length - 1; i >= 0; i--) {
+      if ((nums[r] ** 2 > nums[l] ** 2)) {
+          result[i] = nums[r] ** 2;
+          r--;
+      } else {
+          result[i] = nums[l] ** 2;
+          l++;
+      }
+  }
+
+  return result;
+};
