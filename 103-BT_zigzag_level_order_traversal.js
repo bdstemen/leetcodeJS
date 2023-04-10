765c76597961980c568c0c32e81783c5b07638c0
Ben Stemen <ben.d.stemen@gmail.com>
2023-04-10 09:59:25 -0400

    refactor zigzag level order traversal

--- 103-BT_zigzag_level_order_traversal.js
+++ 103-BT_zigzag_level_order_traversal.js
@@ -1,22 +1,18 @@
 var zigzagLevelOrder = function(root) {
     let result = [];
-    let queue = [root];
-    let row = 0;
+    let queue = root ? [root] : [];
 
     while (queue.length) {
         let level = [];
         let len = queue.length;
         for (let i = 0; i < len; i++) {
             let node = queue.shift();
-            if (!node) continue;
             level.push(node.val);
-            queue.push(node.left);
-            queue.push(node.right);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
         }
-        if (!level.length) continue;
-        else if (row % 2 === 0) result.push(level);
+        if (result.length % 2 === 0) result.push(level);
         else result.push(level.reverse());
-        row++;
     }
 
     return result;
