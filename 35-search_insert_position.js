1adf9706bc4d759eee22a0c19718e9ae9e3f825c
Ben Stemen <ben.d.stemen@gmail.com>
2023-04-12 11:21:53 -0400

    search insert position using binary search

--- 35-search_insert_position.js
+++ 35-search_insert_position.js
@@ -7,3 +7,23 @@ var searchInsert = function(nums, target) {
     }
     return nums.length;
 };
+
+// O(logn) time
+var searchInsert = function(nums, target) {
+    let l = 0;
+    let r = nums.length - 1;
+
+    while (l <= r) {
+        let m = Math.floor((l + r) / 2);
+
+        if (nums[m] === target) {
+            return m;
+        } else if (nums[m] > target) {
+            r = m - 1;
+        } else {
+            l = m + 1;
+        }
+    }
+
+    return l;
+};
