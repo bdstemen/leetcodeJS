231d358c126fb6993eee333297a932b0edbede50
Ben Stemen <ben.d.stemen@gmail.com>
2023-04-18 17:20:04 -0400

    linked list intersection optimized solution

--- 160-intersection_of_two_LL.js
+++ 160-intersection_of_two_LL.js
@@ -53,3 +53,15 @@ var getListLength = function(head) {
 
   return counter
 }
+
+// super clean O(n + m) solution
+var getIntersectionNode = function(headA, headB) {
+  let [l1, l2] = [headA, headB];
+
+  while (l1 !== l2) {
+    l1 = l1 ? l1.next : headB;
+    l2 = l2 ? l2.next : headA;
+  }
+
+  return l1;
+};
