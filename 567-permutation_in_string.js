42a5733bb3bef3c206d48dcfae204b00bf5392d9
Ben Stemen <ben.d.stemen@gmail.com>
2023-04-21 16:52:05 -0400

    int to roman

--- 567-permutation_in_string.js
+++ 567-permutation_in_string.js
@@ -1,30 +1,32 @@
 // O(n * m) time
-var checkInclusion = function(s1, s2) {
-  let s1Chars = {};
-  let windowChars = {};
-  let l = 0;
-  let r = s1.length - 1;
-
-  for (let i = 0; i < s1.length; i++) {
-      s1Chars[s1[i]] = s1Chars[s1[i]] + 1 || 1;
-      windowChars[s2[i]] = windowChars[s2[i]] + 1 || 1;
+var intToRoman = function(num) {
+  const unitsArr = [['I', 'V'], ['X', 'L'], ['C', 'D'], ['M']];
+  let result = '';
+  while(num > 0) {
+    const index = Math.floor(Math.log10(num));
+    const units = unitsArr[index];
+    const currentNum = Math.floor(num / Math.pow(10, index));
+    if (currentNum < 4) {
+      for(i = 1; i <= currentNum; i++) {
+        result = result + units[0];
+      }
+    } else if (currentNum === 4) {
+      result = result + units[0] + units[1];
+    } else if (currentNum === 5) {
+      result = result + units[1];
+    } else if (currentNum < 9) {
+      result = result + units[1];
+      for(i = 1; i <= currentNum - 5; i++) {
+        result = result + units[0];
+      }
+    } else {
+      const upUnits = unitsArr[index + 1];
+      result = result + units[0] + upUnits[0];
+    }
+    if (index === 0) {
+      break;
+    }
+    num = num % Math.pow(10, index);
   }
-
-  while (r < s2.length) {
-      if (isPermutation(s1Chars, windowChars)) return true;
-
-      r++;
-      windowChars[s2[r]] = windowChars[s2[r]] + 1 || 1;
-      windowChars[s2[l]]--;
-      l++
-  }
-
-  return false;
+  return result;
 };
-
-var isPermutation = function(key, curr) {
-  for (char in key) {
-      if (curr[char] !== key[char]) return false;
-  }
-  return true;
-}
