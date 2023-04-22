4bf1be9abf072886f20927a58bb7ffc0518182dc
Ben Stemen <ben.d.stemen@gmail.com>
2023-04-22 16:08:40 -0400

    optimal majority element answer

--- 169-majority_element.js
+++ 169-majority_element.js
@@ -7,3 +7,26 @@ var majorityElement = function(nums) {
         if (numCount[nums[i]] > nums.length / 2) return nums[i];
     }
 };
+
+// O(n) time, O(1) space
+var majorityElement = function(nums) {
+  let i = 0;
+  let result;
+
+  while (i < nums.length) {
+      result = nums[i];
+      let count = 1;
+      i++;
+
+      while (count > 0 && i < nums.length) {
+          if (nums[i] === result) {
+              count++;
+          } else {
+              count--;
+          }
+          i++;
+      }
+  }
+
+  return result;
+};
