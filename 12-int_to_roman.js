6f2a2de8f0ff70784129c408f3f2b45ee0166ec9
Ben Stemen <ben.d.stemen@gmail.com>
2023-04-24 11:53:06 -0400

    optimize int to roman

--- 12-int_to_roman.js
+++ 12-int_to_roman.js
@@ -45,3 +45,39 @@ var intToRoman = function(num) {
 
     return result;
 };
+
+
+
+var intToRoman = function(num) {
+    let result = '';
+
+    let values = [
+        [1, 'I'],
+        [4, 'IV'],
+        [5, 'V'],
+        [9, 'IX'],
+        [10, 'X'],
+        [40, 'XL'],
+        [50, 'L'],
+        [90, 'XC'],
+        [100, 'C'],
+        [400, 'CD'],
+        [500, 'D'],
+        [900, 'CM'],
+        [1000, 'M']
+    ];
+
+
+    for (let i = values.length - 1; i >= 0; i--) {
+        let val = values[i][0];
+        let sym = values[i][1];
+
+        if (Math.floor(num / val)) {
+            let count = Math.floor(num / val);
+            result += sym.repeat(count);
+            num = num % val;
+        }
+    }
+
+    return result;
+};
