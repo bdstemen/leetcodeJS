810bf5db4dc0c5935b83eb4c18e813308776e29e
Ben Stemen <ben.d.stemen@gmail.com>
2023-04-24 11:36:05 -0400

    int to roman

--- 12-int_to_roman.js
+++ 12-int_to_roman.js
@@ -1,29 +1,47 @@
 var intToRoman = function(num) {
-  const arr = [1000, 500, 100,50,10,5,1];
-  const symbols = ['M', 'D','C','L','X','V','I'];
-  let res = '';
-  for(let i=0; i<arr.length; i++){
-      res+=symbols[i].repeat(Math.floor(num/arr[i]));
-      num = num%arr[i];
-      if(i==0 && num>=900){
-          res+='CM';
-          num -=900;
-      }else if(i==1 && num>=400){
-          res+='CD';
-          num-=400;
-      }else if(i==2 && num>=90){
-          res+='XC';
-          num-=90;
-      }else if(i==3 && num>=40){
-          res+='XL';
-          num-=40
-      }else if(i==4 && num>=9){
-          res+='IX';
-          num-=9
-      }else if(i==5 && num>=4){
-          res+='IV';
-          num-=4
-      }
-  }
-  return res;
+    let result = '';
+    let mult = 1;
+
+    let values = {
+        1: 'I',
+        5: 'V',
+        10: 'X',
+        50: 'L',
+        100: 'C',
+        500: 'D',
+        1000: 'M'
+    };
+
+    while (num > 0) {
+        let digitStr = ''
+        let digit = num % 10;
+        num = Math.trunc(num / 10);
+
+        if (digit === 9) {
+            digitStr += values[1 * mult];
+            digitStr += values[10 * mult];
+            digit -= 9;
+        }
+
+        if (digit >= 5) {
+            digitStr += values[5 * mult];
+            digit -= 5;
+        }
+
+        if (digit === 4) {
+            digitStr += values[1 * mult];
+            digitStr += values[5 * mult];
+            digit -= 4;
+        }
+
+        while (digit > 0) {
+            digitStr += values[1 * mult];
+            digit -= 1;
+        }
+
+        mult *= 10
+        result = digitStr + result;
+    }
+
+    return result;
 };
