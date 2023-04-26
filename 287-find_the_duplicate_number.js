094a3d95a4d32b62fe365dd02b57ceeb60e478b7
Ben Stemen <ben.d.stemen@gmail.com>
2023-04-26 14:01:43 -0400

    find duplicate number using flowds algo

--- 287-find_the_duplicate_number.js
+++ 287-find_the_duplicate_number.js
@@ -31,3 +31,22 @@ var findDuplicate = function(nums) {
         }
     }
 };
+
+// floyd's algo solution
+var findDuplicate = function(nums) {
+    let slow = fast = 0;
+
+    while (true) {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+        if (slow === fast) break;
+    }
+
+    let slow2 = 0
+
+    while (true) {
+        slow = nums[slow];
+        slow2 = nums[slow2];
+        if (slow === slow2) return slow;
+    }
+};
