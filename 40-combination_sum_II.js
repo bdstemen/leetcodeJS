28102d70b90d42d8a4ae618c11f332557503b1d0
Ben Stemen <ben.d.stemen@gmail.com>
2023-04-27 11:39:12 -0400

    combination sum II

---
+++ 40-combination_sum_II.js
@@ -0,0 +1,24 @@
+var combinationSum2 = function(candidates, target) {
+    let combinations = [];
+    candidates = candidates.sort((a, b) => (a - b))
+
+    const backtrack = function(combination, sum, i) {
+        if (sum === target) {
+            combinations.push(combination);
+            return;
+        } else if (sum > target || i === candidates.length) {
+            return;
+        }
+
+        backtrack(combination.concat(candidates[i]), sum + candidates[i], i + 1);
+
+        let j = i;
+        while (j < candidates.length && candidates[i] === candidates[j]) {
+            j++;
+        }
+        backtrack(combination, sum, j);
+    }
+
+    backtrack([], 0, 0);
+    return combinations;
+};
