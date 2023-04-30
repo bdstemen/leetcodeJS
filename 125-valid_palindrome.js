6bfa31d36d2ea32f5e5a1f70cac02c5c66ea8892
Ben Stemen <ben.d.stemen@gmail.com>
2023-04-30 19:57:58 -0400

    valid palindrome sub-optimal solution

--- 125-valid_palindrome.js
+++ 125-valid_palindrome.js
@@ -1,16 +1,29 @@
 // O(n) time, O(n) space
- var isPalindrome = function(s) {
-  if (s.length < 2) return true;
-  s = s.toLowerCase().replace(/[^a-z0-9]/gi,'');
+var isPalindrome = function(s) {
+  let filteredString = s.toLowerCase().split('').filter(char => (/[0-9a-z]/).test(char)).join('');
+  let filteredReversedString = filteredString.split('').reverse().join('');
+
+  return filteredString === filteredReversedString;
+};
+
+// optimized solution - O(n) time O(1) space
+var isPalindrome = function(s) {
   let l = 0;
   let r = s.length - 1;
 
   while (l < r) {
-      if (s[l] !== s[r]) return false;
+      if (!(/[0-9a-zA-Z]/).test(s[l])) {
+          l++;
+          continue;
+      } else if (!(/[0-9a-zA-Z]/).test(s[r])) {
+          r--;
+          continue;
+      }
+
+      if (s[l].toLowerCase() !== s[r].toLowerCase()) return false;
       l++;
       r--;
   }
 
   return true;
 };
-
