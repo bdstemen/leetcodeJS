93acc82f703484929a791e2e0d12137e02e0a165
Ben Stemen <ben.d.stemen@gmail.com>
2023-05-01 10:40:33 -0400

    selection sort

--- sorting_algos.js
+++ sorting_algos.js
@@ -16,6 +16,18 @@ const selectionSort = function(arr) {
   return arr;
 }
 
+const insertionSort = function(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let j = i;
+    while (arr[j] < arr[j - 1]) {
+      [arr[j - 1], arr[j]] = [arr[j], arr[j - 1]];
+      j--;
+    }
+  }
+
+  return arr;
+}
+
 const quickSort = function(arr) {
   if (arr.length <= 1) return arr;
 
