b41a269071a542deaca29c36d321320419a4dfd9
Ben Stemen <ben.d.stemen@gmail.com>
2023-05-04 12:33:53 -0400

    jump game dp

--- 55-jump_game.js
+++ 55-jump_game.js
@@ -1,5 +1,19 @@
 // DP approach
+var canJump = function(nums) {
+    let dp = new Array(nums.length).fill(false);
+    dp[0] = true;
 
+    for (let i = 1; i < nums.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if (dp[j] && (j + nums[j]) >= i) {
+                dp[i] = true;
+                break;
+            }
+        }
+    }
+
+    return dp[nums.length - 1];
+};
 
 // greedy solution: O(n) time O(1) space
 var canJump = function(nums) {
